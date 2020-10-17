dd134909fefceecae70a6e1b533b888770b15ab4
MD. Mehedi Hasan <53508562+MehediMubin@users.noreply.github.com>
2020-10-17 16:15:40 +0600

    Update Sieve(Prime).cpp

--- Sieve(Prime).cpp
+++ Sieve(Prime).cpp
@@ -4,7 +4,7 @@ void Sieve (int n)
 {
     prime[0] = prime[1] = 1;
     for (int i = 4; i <= n; i += 2) prime[i] = 1;
-    int limit = sqrt(n) + 1;
+    int limit = sqrt(n + 1);
     for (int i = 3; i <= limit; i += 2) {
         if (prime[i] == 0) {
             for (int j = i * i; j <= n; j += i) prime[j] = 1;
