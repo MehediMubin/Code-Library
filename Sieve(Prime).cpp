2332351d5b57e391ef10dae34ba64189290938a3
MD. Mehedi Hasan <53508562+MehediMubin@users.noreply.github.com>
2021-09-02 10:00:30 +0600

    Update Sieve(Prime).cpp

--- Sieve(Prime).cpp
+++ Sieve(Prime).cpp
@@ -1,15 +1,15 @@
 // O(N*log(log(N)))
 
-bool prime[MAX + 5];
+bitset <N> prime;
 
-void Sieve (int n)
+void Sieve ()
 {
-    prime[0] = prime[1] = 1;
-    for (int i = 4; i <= n; i += 2) prime[i] = 1;
-    int limit = sqrt(n + 1);
+    prime.set();
+    for (int i = 4; i <= N; i += 2) prime[i] = 0;
+    int limit = sqrt(N + 1);
     for (int i = 3; i <= limit; i += 2) {
-        if (prime[i] == 0) {
-            for (int j = i * i; j <= n; j += i) prime[j] = 1;
+        if (prime[i]) {
+            for (int j = i * i; j <= N; j += i) prime[j] = 0;
         }
     }
 }
