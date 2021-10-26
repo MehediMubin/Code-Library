c79e827945a7c24f732ca0dfd8bb456a99137286
MD. Mehedi Hasan <53508562+MehediMubin@users.noreply.github.com>
2021-10-26 16:24:45 +0600

    Update CPTemplate(Basic).cc

--- CPTemplate(Basic).cc
+++ CPTemplate(Basic).cc
@@ -1,20 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#define nl "\n"
+#define endl "\n"
 #define ll long long
-#define pb emplace_back
+#define pb push_back
 #define all(v) (v).begin(),(v).end()
-#define Fread freopen("in.txt","r", stdin)
-#define Fwrite freopen("out.txt", "w", stdout)
-#define what_is(x) cerr << #x << " is " << x << endl;
-#define FastIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define debug(x) cout << #x << " is " << x << endl;
+#define FastIO ios_base::sync_with_stdio(0); cin.tie(0)
 
 int main ()
 {
     FastIO;
-    //Fread;
-    //Fwrite;
 
 
 
