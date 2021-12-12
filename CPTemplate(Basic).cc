a24767c15bfcc817ec63c25f6c9d6d62d55dd474
MD. Mehedi Hasan <53508562+MehediMubin@users.noreply.github.com>
2021-12-12 17:47:19 +0600

    Update CPTemplate(Basic).cc

--- CPTemplate(Basic).cc
+++ CPTemplate(Basic).cc
@@ -2,10 +2,6 @@
 using namespace std;
 
 #define endl "\n"
-#define ll long long
-#define pb push_back
-#define all(v) (v).begin(),(v).end()
-#define debug(x) cout << #x << " is " << x << endl;
 #define FastIO ios_base::sync_with_stdio(0); cin.tie(0)
 
 int main ()
